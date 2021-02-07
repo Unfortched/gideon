01ee4144a5685b6a2c6ed059b558f87788c0ee40
Unfortched <kilovat42@Yandex.ru>
2021-02-07 17:24:37 +0500

    C# Internet information program.
    Program for packages count and ip searching.

---
+++ TCMIP/TCMIP/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: Unfortched
+ * Дата: 25.09.2016
+ * Время: 17:08
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace TCMIP
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
